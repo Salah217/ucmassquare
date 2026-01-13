aaf6d276b737172e13daada2a42bce8aaba1b11f
Farazdag Babikir <farazdag123@gmail.com>
2026-01-14 04:21:56 +0800

    edit fixing ..making changes in view portal in step 2 return

--- ucmas_portal/settings.py
+++ ucmas_portal/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
 
